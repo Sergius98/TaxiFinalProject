59554593063eedda6e9d887866f5dc1ff82c8ecd
sergius <bigsergey1998@gmail.com>
2019-04-19 20:01:02 +0300

    added EncodingFilter

---
+++ src/main/java/com/training/controller/filter/EncodingFilter.java
@@ -0,0 +1,28 @@
+package com.training.controller.filter;
+
+import com.training.controller.servlet.IServletConstants;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = IServletConstants.ENCODING_FILTER_PATH)
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws ServletException, IOException {
+        servletResponse.setContentType("text/html");
+        servletResponse.setCharacterEncoding("UTF-8");
+        servletRequest.setCharacterEncoding("UTF-8");
+
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
